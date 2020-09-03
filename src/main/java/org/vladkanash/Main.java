eed0543ee81261ea5a73bc2692a211561bd1bd8e
Vlad Kanash <vlad.kanash@gmail.com>
2020-09-04 00:02:03 +0300

    Use cron4j to schedule report generation

--- src/main/java/org/vladkanash/Main.java
+++ src/main/java/org/vladkanash/Main.java
@@ -1,13 +1,11 @@
 package org.vladkanash;
 
+import it.sauronsoftware.cron4j.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vladkanash.facade.SlackFacade;
 
 import java.lang.invoke.MethodHandles;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Main {
 
@@ -24,18 +22,15 @@ public class Main {
     }
 
     private static void scheduleReportTask(SlackFacade slackFacade) {
-        var task = createTask(slackFacade);
-        var period = Long.parseLong(context.getConfig().get("slack.reportRate"));
-        new Timer(false).scheduleAtFixedRate(task, new Date(), period);
+        var scheduler = new Scheduler();
+        scheduler.setDaemon(false);
+        var cronExpression = context.getConfig().get("slack.reportSchedule");
+        scheduler.schedule(cronExpression, () -> sendReport(slackFacade));
+        scheduler.start();
     }
 
-    private static TimerTask createTask(SlackFacade slackFacade) {
-        return new TimerTask() {
-            @Override
-            public void run() {
-                LOG.info("Sending current week report...");
-                slackFacade.sendCurrentWeekReport();
-            }
-        };
+    private static void sendReport(SlackFacade slackFacade) {
+        LOG.info("Sending current week report...");
+        slackFacade.sendCurrentWeekReport();
     }
 }
